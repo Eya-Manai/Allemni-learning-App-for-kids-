992439c00c9f2292d1f59e01e3d0ac122bbc75d8
EYA MANAI <eyamanai35@outlook.com>
2025-06-27 14:57:31 +0200

    routing reset pass

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,16 +1,20 @@
+import 'package:allemni/interfaces/forgotpassword.dart';
 import 'package:allemni/interfaces/login.dart';
 import 'package:allemni/interfaces/parenthome.dart';
 import 'package:allemni/interfaces/signup.dart';
+
 import 'package:flutter/material.dart';
 
 class Routes {
   static const String signup = '/signup';
   static const String login = '/login';
   static const String parentHome = '/parentHome';
+  static const String forgetPassword = '/forgetpassword';
 
   static Map<String, WidgetBuilder> routes = {
     login: (context) => const LoginInterface(),
     signup: (context) => const Signup(),
     parentHome: (context) => const Parenthome(),
+    forgetPassword: (context) => const Forgotpassword(),
   };
 }
