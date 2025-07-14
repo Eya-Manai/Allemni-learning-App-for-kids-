b0427d4acf62320c43c2348e0d8535bf3f52470b
EYA MANAI <eyamanai35@outlook.com>
2025-07-14 05:07:12 +0200

    starting choose module interface

--- lib/interfaces/choose_module.dart
+++ lib/interfaces/choose_module.dart
@@ -2,6 +2,7 @@ import 'package:allemni/widgets/childnavbar.dart';
 import 'package:allemni/widgets/draw_background.dart';
 import 'package:allemni/widgets/draw_title.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ChooseModule extends StatefulWidget {
   const ChooseModule({super.key});
@@ -11,6 +12,32 @@ class ChooseModule extends StatefulWidget {
 }
 
 class ChooseModuleState extends State<ChooseModule> {
+  String childId = "";
+  String subjectId = "";
+  String subjectName = "";
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final args =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
+    subjectId = args['subjectId'] ?? "";
+    subjectName = args['subjectName'] ?? "";
+    loadChildId();
+  }
+
+  Future<void> loadChildId() async {
+    try {
+      final preferences = await SharedPreferences.getInstance();
+
+      setState(() {
+        childId = preferences.getString("childId") ?? "";
+      });
+    } catch (e) {
+      throw Exception("Error loading childId from SharedPreferences");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,11 +45,27 @@ class ChooseModuleState extends State<ChooseModule> {
       body: Stack(
         children: [
           Buildbackground(),
-          BuildTitle("اختر الوحدة"),
-          Center(
-            child: Text(
-              "هذه الصفحة مخصصة لاختيار الوحدة",
-              style: TextStyle(fontSize: 20, color: Colors.black54),
+          Directionality(
+            textDirection: TextDirection.rtl,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                BuildTitle("اختر المادة"),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 6, // Number of modules
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text("Module ${index + 1}"),
+                        onTap: () {
+                          // Handle module selection
+                        },
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
           ),
         ],
