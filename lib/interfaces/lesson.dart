c25dd821428e34a1b06f6d514439150eee78c94e
EYA MANAI <eyamanai35@outlook.com>
2025-07-30 23:51:54 +0200

    starting designing the lesson page

--- lib/interfaces/lesson.dart
+++ lib/interfaces/lesson.dart
@@ -1,3 +1,4 @@
+import 'package:allemni/constants/colors.dart';
 import 'package:allemni/models/lesson_model.dart';
 import 'package:allemni/widgets/childnavbar.dart';
 import 'package:flutter/material.dart';
@@ -11,18 +12,70 @@ class Lesson extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ChildNavbar(),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Text(
-                lesson.name,
-                style: TextStyle(fontSize: 22, fontFamily: 'childFont'),
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          return SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: BoxConstraints(minHeight: constraints.maxHeight),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: Text(
+                        lesson.name,
+                        style: TextStyle(fontSize: 24, fontFamily: 'childFont'),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    height: 200,
+
+                    decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.play_circle_fill,
+                        size: 80,
+                        color: AppColors.primaryYellow,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.6,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(horizontal: 0),
+                      padding: EdgeInsets.symmetric(horizontal: 0),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage(
+                            "assets/images/lessonbackground.jpg",
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                        color: AppColors.white,
+                        borderRadius: BorderRadius.circular(2),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black12,
+                            blurRadius: 8,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
